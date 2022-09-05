95c03c0c88db02e74f6a44ecb5882cd963e32244
Jacob McEwan S3841062 <s3841062@student.rmit.edu.au>
2022-09-05 17:38:25 +1000

    fix: :bug: Fix missing dep in package step
    Added the missing websoket-sharp dependency to the package step

--- package.ps1
+++ package.ps1
@@ -1,5 +1,6 @@
+Remove-Item ".\GalacticComms Plugin.zip"
 $compress = @{
-  Path = ".\bin\Debug\GalacticComms.dll", ".\bin\Debug\GalacticComms.pdb", ".\manifest.xml"
+  Path = ".\bin\Debug\GalacticComms.dll", ".\bin\Debug\GalacticComms.pdb", ".\bin\Debug\websocket-sharp.dll", ".\bin\Debug\websocket-sharp.pdb", ".\manifest.xml"
   CompressionLevel = "Fastest"
   DestinationPath = ".\GalacticComms Plugin.zip"
 }
